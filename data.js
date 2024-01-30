507f20a7a33aa5ec2448309034d4911e4e8024d1
Preston <JACKSONP11@mymail.nku.edu>
2024-01-30 14:10:42 -0500

    data.js updated with arrays

---
+++ data.js
@@ -0,0 +1,12 @@
+let Animal_Data = [
+    ['Baxter','Dog','Beagle','Male','Tri-Color(Black, Brown, White)', 'Yes','5 Years','34621','982000364789210'],
+    ['Luna','Cat','Maine Coon','Female','Grey with white markings','Yes','3 Years','57281','981120019776432'],
+    ['Sandy','Dog','Golden Retriever','Female','Golden','Yes','7 Years','41382','985112004567890'],
+    ['Oliver','Cat','Siamese','Male','Seal Point','Yes','2 Years','62894','981120025678902'],
+    ['Max','Dog','Labrador Retriever','Male','Chocolate','Yes','4 Years','47920','985120031478902'],
+    ['Daisy','Cat','Persian','Female','White','Yes','6 Years','58329','981120036712345'],
+    ['Charlie','Dog','Dachshund','Dapple(Black and Gray)','Yes','3 Years','49583','982000367829101'],
+    ['George','Cat','British Shorthair','Male','Blue(Gray)','Yes','4 Years','61029','985112007651234'],
+    ['Bella','Dog','Boxer','Female','Fawn with White Markings','Yes','6 Years','50824','982000360234567'],
+    ['Sophie','Cat','Ragdoll','Female','Seal Point with Blue Eyes','Yes','1 Year','63845','985112008765432'],
+];
