6cfdc15459f2eb5cd0a67ca1d286c3d954df3b7c
Preston <JACKSONP11@mymail.nku.edu>
2024-01-30 20:25:44 -0500

    add all images and input for i

--- data.js
+++ data.js
@@ -43,7 +43,7 @@ let Animal_Data = [
         id:'62894',
         microchip:'981120025678902',
         about:"Oliver is a sleek Siamese cat with striking blue eyes and a seal point coat. Found wandering in a neighborhood, he's adapted well to shelter life but is eager for a permanent home. Oliver is vocal, expressing his thoughts freely, and enjoys interactive playtime. He's affectionate and loves to be close to his human companions, often following them around the house. Oliver would do best in a home where he can receive plenty of attention and love.",
-        image: ''
+        image: '../images/OliverCat.jpg'
         },
 
     { name:'Max',
@@ -55,7 +55,7 @@ let Animal_Data = [
         id:'47920',
         microchip:'985120031478902',
         about:"Max is a charming Chocolate Labrador with a love for life and a tail that never stops wagging. Rescued from an overcrowded shelter, Max has shown immense gratitude and loyalty to his caregivers. He is full of energy and would thrive in a home with a backyard or regular access to outdoor activities. Max is great with children and other dogs, and he's always up for a game of fetch or a leisurely walk. His ideal home would be with an active family who can match his energy and zest for life.",
-        image:''
+        image:'../images/MaxDog.jpg'
         },
 
     { name:'Daisy',
@@ -67,7 +67,7 @@ let Animal_Data = [
         id:'58329',
         microchip:'981120036712345',
         about:"Daisy is a stunning white Persian cat with a luxurious coat and gentle demeanor. She was surrendered by an owner who could no longer care for her. Daisy is a bit of a diva and loves being the center of attention. She enjoys being groomed and will happily sit in your lap for hours. She's not fond of loud noises or fast movements, so a quiet and calm household would be ideal. Daisy is looking for a loving home where she can be pampered and adored.",
-        image:''
+        image:'../images/DaisyCat.jpg'
         },
 
     { name:'Charlie',
@@ -79,7 +79,7 @@ let Animal_Data = [
         id:'49583',
         microchip:'982000367829101',
         about:"Charlie is a spirited Dapple Dachshund with a playful personality. He was found as a stray and has since shown an incredible zest for life. Charlie loves exploring, whether it's a new trail or the corners of his home. He's a bit mischievous and enjoys hiding his toys. Charlie would do well in a home with an individual or family who enjoys fun and adventure. He's small but has a big heart and a lot of love to give.",
-        image:''
+        image:'../images/CharlieDog.jpg'
         },
 
    { name:'George',
@@ -91,7 +91,7 @@ let Animal_Data = [
        id:'61029',
        microchip:'985112007651234',
        about:"George enjoys leisurely days, lounging in sunny spots, and watching the world go by. He's not overly demanding for attention but appreciates a good chin scratch and will purr loudly in response. George is a bit reserved at first but becomes a loyal companion once he feels comfortable. He would be perfect for someone seeking a low- maintenance, independent cat.",
-        image:''
+        image:'../images/GeorgeCat.jpg'
        },
 
    { name:'Bella',
@@ -103,7 +103,7 @@ let Animal_Data = [
        id:'50824',
        microchip:'982000360234567',
        about:"Bella is a spirited Boxer with a fawn coat and charming white markings. She was rescued from a neglectful situation and has shown immense love and gratitude to her caregivers. Bella is energetic and playful, enjoying activities like running and playing fetch. She's great with older children and would thrive in a home where she can be active and engaged. Bella has a big personality and would be a fantastic companion for someone who loves adventure and fun.",
-        image:''
+        image:'../images/BellaDog.jpg'
        },
 
    { name:'Sophie',
@@ -115,10 +115,11 @@ let Animal_Data = [
        id:'63845',
        microchip:'985112008765432',
        about:"Sophie is a young Ragdoll cat with a soft seal point coat and mesmerizing blue eyes. She was found as a kitten and raised in a foster home. Sophie is incredibly affectionate, often seeking out laps to curl up in. She's playful and enjoys interactive toys, but she's also content to relax and watch the world go by. Sophie gets along well with other pets and would be an excellent addition to a loving family or individual looking for a gentle and loving companion.",
-        image:''
+        image:'../images/SophieCat.jpg'
        }
 ];
-var i = 2;
+var i = getQueryParam('i');
+
 var name = Animal_Data[i].name;
 document.getElementById("name").innerHTML = name;
 
@@ -151,3 +152,8 @@ function buildImage() {
     img.src = Animal_Data[i].image;
     document.getElementById('content').appendChild(img);
 }
+
+function getQueryParam(key){
+			let queryParams=new URLSearchParams(window.location.search);
+			return queryParams.has(key) ? queryParams.get(key) : null;
+		}
